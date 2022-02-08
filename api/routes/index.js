3fe5dca26ec5cd84ab32ed35160cc54d51574992
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-02-08 12:51:30 -0500

    Changes to readme file, some changes to ci script

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,3 +1,7 @@
+/**
+ * performs a Http get request
+ * @param {app} the express app 
+ */
 module.exports = function(app) {
     app.get('/healthz', (req, res) => {
         res.send('Health endpoint');
