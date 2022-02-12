3c90126fe66cde6158d22cf5ff63e1a21edb8b67
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-02-12 11:18:05 -0500

    Simple Database connection successful

--- api/app.js
+++ api/app.js
@@ -3,9 +3,15 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const routes = require('./routes/index')
 const cors = require('cors')
+const db = require('../config/database')
 
 // creates an express server
 const app = express();
+
+// Test db
+db.authenticate()
+    .then(() => console.log('Database Connected...'))
+    .catch(err => console.log('Error:' +err))
 
 app.use(cors());
 app.use(express.json());
---
+++ config/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+//connect to postgres server
+const db = new Sequelize('testdb', 'postgres', 'rolwyn12345', {
+    host:  'localhost',
+    dialect: 'postgres',
+    operatorsAlias: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+})
+
+module.exports = db;
