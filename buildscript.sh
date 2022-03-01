3d8b7b6cdca8216d5cb8472baa1836f70c9f20f9
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-03-01 14:32:08 -0500

    testing ci

--- buildscript.sh
+++ buildscript.sh
@@ -12,63 +12,66 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 # Install node version
 nvm install node
+nvm use 16
+node --version
 
-sudo yum install postgresql12 postgresql12-server
+# sudo yum install postgresql12 postgresql12-server
 
-sudo amazon-linux-extras | grep postgre
+# sudo amazon-linux-extras | grep postgre
 
-# get the required version
-sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
-[pgdg12]
-name=PostgreSQL 12 for RHEL/CentOS 7 - x86_64
-baseurl=https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-7-x86_64
-enabled=1
-gpgcheck=0
-EOF
+# # get the required version
+# sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
+# [pgdg12]
+# name=PostgreSQL 12 for RHEL/CentOS 7 - x86_64
+# baseurl=https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-7-x86_64
+# enabled=1
+# gpgcheck=0
+# EOF
 
-# install version 12
-sudo yum install -y postgresql12 postgresql12-server
+# # install version 12
+# sudo yum install -y postgresql12 postgresql12-server
 
-# init db
-sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+# # init db
+# sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
 
-# enables and start service with --now
-sudo systemctl enable --now postgresql-12
+# # enables and start service with --now
+# sudo systemctl enable --now postgresql-12
 
-systemctl status postgresql-12
+# systemctl status postgresql-12
 
-# cd ~/webservice
+# # cd ~/webservice
 
-# sudo -u postgres psql testdb
-# psql -c "ALTER USER postgres PASSWORD 'rolwyn12345';"
+# # sudo -u postgres psql testdb
+# # psql -c "ALTER USER postgres PASSWORD 'rolwyn12345';"
 
-# echo "ALTER USER postgres PASSWORD 'rolwyn12345'" > rst_pass.sql
+# # echo "ALTER USER postgres PASSWORD 'rolwyn12345'" > rst_pass.sql
 
+# # sudo su -
+# # sudo chmod 755 /home/ec2-user
+# # sudo psql --command='create ROLE "ec2-user"'
+# # sudo su - postgres
+# # psql -c "ALTER USER postgres PASSWORD 'rolwyn12345';"
+# # # sudo -u postgres psql -d testdb
+# # # sudo -u postgres createdb testdb
+# # sudo -u postgres psql -d testdb
+# # psql -c "select * from testdb;"
+
+# # sudo su - postgres
 # sudo su -
 # sudo chmod 755 /home/ec2-user
-# sudo psql --command='create ROLE "ec2-user"'
-# sudo su - postgres
-# psql -c "ALTER USER postgres PASSWORD 'rolwyn12345';"
-# # sudo -u postgres psql -d testdb
-# # sudo -u postgres createdb testdb
-# sudo -u postgres psql -d testdb
-# psql -c "select * from testdb;"
-
-# sudo su - postgres
-sudo su -
-sudo chmod 755 /home/ec2-user
-sudo -u postgres psql
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'rolwyn12345';"
-sudo -u postgres psql -c "CREATE DATABASE testdb;"
+# sudo -u postgres psql
+# sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'rolwyn12345';"
+# sudo -u postgres psql -c "CREATE DATABASE testdb;"
 # sudo -u postgres psql -c "DROP TABLE IF EXISTS users;"
 
 
-cd ~
+# cd ~
 # sudo rm -f webservice/package-lock.json
+ls -a
 cd ./webservice
 ls -a
 # rm -f ./package-lock.json
-npm install
+# npm install
 
 # sudo npm install
 # sudo npm run start
