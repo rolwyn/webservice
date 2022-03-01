03f7cc84f8ecbf9e50b4d659cb2a567d9f6006bf
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-03-01 00:20:11 -0500

    packer workflow test

--- buildscript.sh
+++ buildscript.sh
@@ -40,7 +40,9 @@ sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
 sudo systemctl enable --now postgresql-12
 # sudo systemctl start postgresql-12.service
 
-sudo -u postgres psql
+sudo cd /home/ec2-user
+# sudo -u postgres psql
+sudo su - postgres
 
 # sudo passwd postgres
 
