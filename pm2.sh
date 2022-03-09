aad2177904bdf73b11ed8fc1313650194114c66f
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-03-09 16:51:44 -0500

    test db

--- pm2.sh
+++ pm2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 10
 source /etc/profile.d/webapp.sh
-export DB_ADDRESS=${RDSDBInstance.Endpoint.Address}
-export AWS_BUCKET_NAME=${S3BuckeEncrypted}
-export AWS_BUCKET_REGION=${AWS::Region}
-export DB_NAME=${DBName}
-export DB_PASSWORD=${MasterDBPassword}
-export DB_USER_NAME=${MasterDBUsername}
+export DB_ADDRESS_NEW=${DB_ADDRESS}
+export AWS_BUCKET_NAME_NEW=${AWS_BUCKET_NAME}
+export AWS_BUCKET_REGION_NEW=${AWS_BUCKET_REGION}
+export DB_NAME_NEW=${DB_NAME}
+export DB_PASSWORD_NEW=${DB_PASSWORD}
+export DB_USER_NAME_NEW=${DB_USER_NAME}
 
 sudo npm start /home/ec2-user/webservice/server.js
