7dc3b9af847f5f19a6ddc02ed2f99b3db358c1fb
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-03-09 02:59:48 -0500

    made small error

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 //connect to postgres server
-const db = new Sequelize(process.env.DB_ADDRESS, process.env.DB_USER_NAME, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_PASSWORD, {
     host:   process.env.DB_ADDRESS,
     dialect: 'postgres',
     operatorsAlias: false,
