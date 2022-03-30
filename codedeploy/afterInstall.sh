ece4934d1daa7d66710036d4dbb3fa658aa58f02
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-03-29 21:39:16 -0400

    testing

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,9 +1,9 @@
-echo "After install"
-pwd
-ls -al
-"echo In /home/ec2-user"
-cd /home/ec2-user
-echo "In webservice"
-cd webservice
-echo "npm install dependencies"
-sudo npm install --production
+# echo "After install"
+# pwd
+# ls -al
+# "echo In /home/ec2-user"
+# cd /home/ec2-user
+# echo "In webservice"
+# cd webservice
+# echo "npm install dependencies"
+# sudo npm install --production
