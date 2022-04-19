87b9e8cfac5e244d2b0815ac53a3ea8253f5bf0a
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-04-19 17:18:01 -0400

    testing ssl certificate for rds

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+const fs = require('fs')
 require('dotenv').config()
 //connect to postgres server
 
@@ -12,6 +13,11 @@ const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER_NAME}`
     port: 5432,
     dialect: 'postgres',
     logging: console.log,
+    dialectOptions: {
+        ssl: {
+            ca: fs.readFileSync('/global-bundle.pem').toString()
+        }
+    },
     pool: { maxConnections: 5, maxIdleTime: 30 }
 })
 
